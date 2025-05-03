cef40fa89bcc455ae71a1a57f351122315e8492c
Giuseppe Attanasio <giuseppeattanasio6@gmail.com>
2025-05-03 12:19:24 +0200

    remove cache use to avoid reusing badly formatted requests

--- bash/run_model.sh
+++ bash/run_model.sh
@@ -5,8 +5,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
-#SBATCH --gpus-per-node=4
-#SBATCH --time=04:00:00
+#SBATCH --gpus-per-node=1
+#SBATCH --time=01:00:00
 #SBATCH --mem-per-gpu=32G
 #SBATCH --account=IscrC_CALAMITA
 
@@ -32,14 +32,13 @@ export LOCAL_DATASETS_DIR=$BASE_DIR/local_datasets
 export BLEURT_CHECKPOINT=$BASE_DIR/BLEURT-20
 
 # TODO: set the correct path to your output directory
-OUTPUT_DIR=$BASE_DIR/results_calamita
+OUTPUT_DIR=$BASE_DIR/results_magnet_v4
 
 # TODO: set the correct path to your venv
 source venv/bin/activate
 
 MODEL=$1
 BATCH_SIZE=auto
-
 
 module unload cuda
 module load cuda/12.3
@@ -55,14 +54,12 @@ NUM_GPUS=4
 echo "NUM_GPUS: $NUM_GPUS"
 
 lm_eval --model vllm \
-    --model_args pretrained=${MODEL},dtype=bfloat16,gpu_memory_utilization=0.8,max_model_len=3072,tensor_parallel_size=$NUM_GPUS \
+    --model_args pretrained=${MODEL},dtype=bfloat16,gpu_memory_utilization=0.8,max_model_len=2048,tensor_parallel_size=$NUM_GPUS \
     --tasks ${tasks} \
     --output_path ${OUTPUT_DIR} \
     --batch_size $BATCH_SIZE \
     --log_samples \
     --write_out \
-    --use_cache $OUTPUT_DIR/cache/${MODEL//\//__} \
-    --cache_requests "true" \
     --include_path ./tasks \
     --load_local \
     --local_base_dir $LOCAL_DATASETS_DIR \
